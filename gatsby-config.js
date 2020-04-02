1e4050848cc148120b8e458e0858f1b32704d29a
Marco Chalub <marcochalub@Marcos-MacBook.local>
2020-04-03 10:18:57 +1100

    Config Manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -34,8 +34,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Rohit Gupta`,
-        short_name: `Rohit Gupta`,
+        name: `Intelligent Energy Solutions`,
+        short_name: `IES`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#333`,
