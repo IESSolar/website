efbc86aea9a701c5501dbcbfe775d0db1e621ba8
Marco Chalub <marcochalub@Marcos-MacBook.local>
2020-04-07 07:25:28 +1000

    Page template

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,7 @@
 var path = require("path");
 
-exports.createPages = ({ graphql, boundActionCreators }) => {
-  const { createPage } = boundActionCreators;
+exports.createPages = ({ graphql, actions }) => {
+  const { createPage } = actions;
   return new Promise((resolve, reject) => {
     const blogPostTemplate = path.resolve("src/templates/blog-post.js");
     resolve(
@@ -24,6 +24,41 @@ exports.createPages = ({ graphql, boundActionCreators }) => {
           createPage({
             path: edge.node.slug,
             component: blogPostTemplate,
+            context: {
+              slug: edge.node.slug
+            }
+          });
+        });
+        return;
+      })
+    );
+  });
+};
+
+exports.createPages = ({ graphql, actions }) => {
+  const { createPage } = actions;
+  return new Promise((resolve, reject) => {
+    const menuPageTemplate = path.resolve("src/templates/page.js");
+    resolve(
+      graphql(`
+        {
+          allContentfulPages(limit: 10) {
+            edges {
+              node {
+                id
+                slug
+              }
+            }
+          }
+        }
+      `).then(result => {
+        if (result.errors) {
+          reject(result.errors);
+        }
+        result.data.allContentfulPages.edges.forEach(edge => {
+          createPage({
+            path: edge.node.slug,
+            component: menuPageTemplate,
             context: {
               slug: edge.node.slug
             }
